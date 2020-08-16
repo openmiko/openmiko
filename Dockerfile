18cfafdd1dd59e16f56592f7688291906c6cab37
Jeff Tchang <jeff.tchang@gmail.com>
2020-08-15 19:56:40 -0700

    Add patch so libv4l builds (need -lpthread)

--- Dockerfile
+++ Dockerfile
@@ -23,30 +23,23 @@ RUN \
 
 RUN locale-gen --no-purge en_US.UTF-8
 
-# Copy the entire source tree. Expect it to be mounted over when developing.
-COPY . /src
-
-
-#COPY fs /openmiko/fs
-#COPY legacy_src /openmiko/legacy_src
-#COPY config /openmiko/config
-#COPY patches /openmiko/patches
-#COPY buildscripts /openmiko/buildscripts
-
-
-RUN mkdir -p /openmiko/_build
-WORKDIR "/openmiko/_build"
+RUN mkdir -p /openmiko/build
+WORKDIR "/openmiko/build"
 RUN wget https://buildroot.org/downloads/buildroot-2016.02.tar.gz;
 RUN tar xvf buildroot-2016.02.tar.gz;
 RUN rm buildroot-2016.02.tar.gz;
 
 # Change to the buildroot directory
-WORKDIR "/openmiko/_build/buildroot-2016.02"
+WORKDIR "/openmiko/build/buildroot-2016.02"
+
+
+# Copy the entire source tree. Expect it to be mounted over when developing.
+COPY . /src
 
 
 RUN /src/buildscripts/setup_buildroot.sh
 
-RUN make
+#RUN make
 
 WORKDIR /openmiko
 
--- buildscripts/setup_buildroot.sh
+++ buildscripts/setup_buildroot.sh
@@ -4,12 +4,13 @@ set -e
 # This script is expected to be run inside the development container
 # It copies from /src files that have been changed for buildroot purposes
 
-cd /openmiko/_build/buildroot-2016.02
+cd /openmiko/build/buildroot-2016.02
 
 # Copy over custom packages
-cp -r /src/custompackages/package/* /openmiko/_build/buildroot-2016.02/package/
+cp -r /src/custompackages/package/* /openmiko/build/buildroot-2016.02/package/
 
-patch -p1 < /openmiko/patches/add_fp_no_fused_madd.patch
+patch -p1 < /src/patches/add_fp_no_fused_madd.patch
+patch -p1 < /src/patches/libv4l_add_lpthread.patch
 
 # Replace the default buildroot config files with our custom ones
 cp /src/config/buildroot.config .config
@@ -19,4 +20,3 @@ cp /src/config/uClibc-ng.config package/uclibc
 # We want to use specific sources so copy these into the download directory
 mkdir -p dl
 cp /src/legacy_src/kernel-3.10.14.tar.xz dl/
-cp /src/legacy_src/uboot-v2013.07.tar.xz dl/

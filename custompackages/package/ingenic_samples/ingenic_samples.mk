28c49036c247f2f27cf3bc5327e39517a6f82d04
Jeff Tchang <jeff.tchang@gmail.com>
2020-08-28 20:35:15 -0700

    Make sure ingenic samples works

--- custompackages/package/ingenic_samples/ingenic_samples.mk
+++ custompackages/package/ingenic_samples/ingenic_samples.mk
@@ -5,9 +5,32 @@
 ################################################################################
 
 INGENIC_SAMPLES_VERSION = v0.1.0
-INGENIC_SAMPLES_SOURCE = ingenic-samples.tar.gz
-INGENIC_SAMPLES_SITE = https://github.com/mpromonet/INGENIC_SAMPLES_/archive
+#INGENIC_SAMPLES_SOURCE = ingenic-samples.tar.gz
+INGENIC_SAMPLES_SITE = /src/custompackages/package/ingenic_samples
 INGENIC_SAMPLES_DEPENDENCIES =
 INGENIC_SAMPLES_CONF_OPTS =
+INGENIC_SAMPLES_SITE_METHOD = local
+
+define INGENIC_SAMPLES_BUILD_CMDS
+        $(MAKE) -C $(@D)/libimp-samples TARGET_CROSS="$(TARGET_CROSS)" CXX="$(TARGET_CXX)" LD="$(TARGET_LD)" all
+endef
+
+define INGENIC_SAMPLES_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/libimp-samples/sample-ISP-flip $(TARGET_DIR)/usr/sbin
+	$(INSTALL) -D -m 0755 $(@D)/libimp-samples/sample-Setfps $(TARGET_DIR)/usr/sbin
+	$(INSTALL) -D -m 0755 $(@D)/libimp-samples/sample-Decoder-jpeg $(TARGET_DIR)/usr/sbin
+	$(INSTALL) -D -m 0755 $(@D)/libimp-samples/sample-Encoder-h264 $(TARGET_DIR)/usr/sbin
+	$(INSTALL) -D -m 0755 $(@D)/libimp-samples/sample-Change-Resolution $(TARGET_DIR)/usr/sbin
+	$(INSTALL) -D -m 0755 $(@D)/libimp-samples/sample-OSD $(TARGET_DIR)/usr/sbin
+	$(INSTALL) -D -m 0755 $(@D)/libimp-samples/sample-Snap-Raw $(TARGET_DIR)/usr/sbin
+	$(INSTALL) -D -m 0755 $(@D)/libimp-samples/sample-Encoder-h264-IVS-move $(TARGET_DIR)/usr/sbin
+	$(INSTALL) -D -m 0755 $(@D)/libimp-samples/sample-Encoder-jpeg $(TARGET_DIR)/usr/sbin
+	$(INSTALL) -D -m 0755 $(@D)/libimp-samples/sample-Encoder-h264-jpeg $(TARGET_DIR)/usr/sbin
+	$(INSTALL) -D -m 0755 $(@D)/libimp-samples/sample-Audio $(TARGET_DIR)/usr/sbin
+
+endef
 
 $(eval $(generic-package))
+
+
+

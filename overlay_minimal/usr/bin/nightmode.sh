3288b6e7a86494b48cd891993c383431b4a59ab4
Jameson Ricks <jricks92@gmail.com>
2021-09-27 17:34:04 -0600

    Add writing to file when nightmode is active
    This allows the updated videocapture binary to read the file and
    automatically switch the IMP sensor to night mode

--- overlay_minimal/usr/bin/nightmode.sh
+++ overlay_minimal/usr/bin/nightmode.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+NIGHTVISION_FILE="/tmp/night_vision_enabled"
+
 case $1 in
   on)
-    echo "nightmode on"
-    curl -d value=0 http://127.0.0.1:8081/api/ir_cut
-    curl -d value=1 http://127.0.0.1:8081/api/ir_led
+    echo "$(date) - nightmode on"
+    curl -d value=0 http://127.0.0.1:8081/api/ir_cut 2>/dev/null
+    curl -d value=1 http://127.0.0.1:8081/api/ir_led 2>/dev/null
+    echo "1" > $NIGHTVISION_FILE
     ;;
   off)
-    echo "nightmode off"
-    curl -d value=1 http://127.0.0.1:8081/api/ir_cut
-    curl -d value=0 http://127.0.0.1:8081/api/ir_led
+    echo "$(date) - nightmode off"
+    curl -d value=1 http://127.0.0.1:8081/api/ir_cut 2>/dev/null
+    curl -d value=0 http://127.0.0.1:8081/api/ir_led 2>/dev/null
+    echo "0" > $NIGHTVISION_FILE
     ;;
   *)
     ;;

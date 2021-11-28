288487b6592d1dbc650b57fb30f1eded1bed11c8
Jeff Tchang <jeff.tchang@gmail.com>
2021-11-27 17:30:11 -0800

    Buildroot github call changed so reworking packages

--- custompackages/package/micropython-lib/micropython-lib.mk
+++ custompackages/package/micropython-lib/micropython-lib.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-MICROPYTHON_LIB_VERSION = 1.9.3
-MICROPYTHON_LIB_SITE = $(call github,micropython,micropython-lib,v$(MICROPYTHON_LIB_VERSION))
+MICROPYTHON_LIB_VERSION = 2829d4adc968a7208d96fa762a9ab4c1b744f73d # 1.9.3
+MICROPYTHON_LIB_SITE = https://github.com/micropython/micropython-lib.git
+MICROPYTHON_LIB_SITE_METHOD = git
 MICROPYTHON_LIB_LICENSE = Python-2.0 (some modules), MIT (everything else)
 MICROPYTHON_LIB_LICENSE_FILES = LICENSE
 
